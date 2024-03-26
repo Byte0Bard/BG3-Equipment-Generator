ede09e76643eedc68e87347f50cfd9ce58d789db
ByteBard0 <154865396+Byte0Bard@users.noreply.github.com>
2024-03-26 19:09:50 -0400

    Update to 1.4.7
    - Fixed inability to set versatile damage
    - Added Update Messages
    - Added easier file explorer
    - Set default weights for equipment so they have sound
    - Changed the way profiles are saved and loaded. Now they are chosen in the file explorer
    - Fixed an error where copying items that have one material on multiple parts creates material duplicates
    - Fixed .DDS files coming out as .dds
    - Removed false error message "must select body type" when choosing anything that isn't armor
    - Added more GUI refreshes (Hopefully fixing the scratch card GUI issues)
    - Added "Parent Race" in root templates lsx file so Githyanki and other races can wear generated armor
    - Fixed false damage number error message when trying to submit damage
    - Added red "not saved" text if the save fails and "saved" text if it succeeds
    - Added the ability to select multiple races for equipment via check boxes.
    - Updated copying methods to (hopefully) copy without errors
    - Added some comments in lsx files to make manual editing easier

--- NoInstall/app/lib/crunch/bin/convertBG3.bat
+++ NoInstall/app/lib/crunch/bin/convertBG3.bat
@@ -9,9 +9,11 @@ set "dxt1List="
 set /a "totalFiles=0"
 set /a "currentFile=0"
 set /p "folderPath=Choose a folder: " 
+set "renameList="
+set newName=""
 echo "Choice made: !folderPath!"
 folderPath = !folderPath!
-for %%W in ("!folderPath!"\*_BM.png "!folderPath!"\*_NM.png "!folderPath!"\*_BM.jpg "!folderPath!"\*_NM.jpg "!folderPath!"\*_BM.tga "!folderPath!"\*_NM.tga "!folderPath!"\*_BM.bmp "!folderPath!"\*_NM.bmp "!folderPath!"\*_BM.tiff "!folderPath!"\*_NM.tiff "!folderPath!"\*_BCM.png "!folderPath!"\*_BCM.jpg "!folderPath!"\*_BCM.tga "!folderPath!"\*_BCM.bmp "!folderPath!"\*_BCM.tiff "!folderPath!"\*icons.png "!folderPath!"\*_ICON.png "!folderPath!"\*_ICON.jpg "!folderPath!"\*_ICON.tga "!folderPath!"\*_ICON.bmp "!folderPath!"\*_ICON.tiff) do (
+for %%W in ("!folderPath!"\*_BM.png "!folderPath!"\*_NM.png "!folderPath!"\*_BM.jpg "!folderPath!"\*_NM.jpg "!folderPath!"\*_BM.tga "!folderPath!"\*_NM.tga "!folderPath!"\*_BM.bmp "!folderPath!"\*_NM.bmp "!folderPath!"\*_BM.tiff "!folderPath!"\*_NM.tiff "!folderPath!"\*_BCM.png "!folderPath!"\*_BCM.jpg "!folderPath!"\*_BCM.tga "!folderPath!"\*_BCM.bmp "!folderPath!"\*_BCM.tiff "!folderPath!"\*icons.png "!folderPath!"\*_ICON.png "!folderPath!"\*_ICON.jpg "!folderPath!"\*_ICON.tga "!folderPath!"\*_ICON.bmp "!folderPath!"\*_ICON.tiff "!folderPath!"\*_MSKA.png "!folderPath!"\*_MSKA.jpg "!folderPath!"\*_MSKA.tga "!folderPath!"\*_MSKA.bmp "!folderPath!"\*_MSKA.tiff) do (
     echo "Adding file to dxt5 list: %%~W"
 	set dxt5List=!dxt5List! "%%W"
 	set /a "totalFiles+=1"
@@ -26,17 +28,19 @@ for %%X in ("!folderPath!"\*_PM.png "!folderPath!"\*_PM.jpg "!folderPath!"\*_PM.
 echo "Total Files @!totalFiles!"
 
 for %%F in (%dxt5List%) do (
-	set /a "currentFile+=1"
-	echo "Current file #!currentFile!/!totalFiles!"
-	"%batchDir%crunch.exe" -file "%%~F" -outsamedir -fileformat dds -dxt5
-	del %%F
+    set /a "currentFile+=1"
+    echo "Current file #!currentFile!/!totalFiles!"
+    "%batchDir%crunch.exe" -file "%%~F" -outsamedir -fileformat DDS -dxt5
+    del "%%F"
+    ren "%%~dpnF.dds" "%%~nF.DDS"
 )
 
 for %%O in (%dxt1List%) do (
 	set /a "currentFile+=1"
 	echo "Current file #!currentFile!/!totalFiles!"
-	"%batchDir%crunch.exe" -file "%%~O" -outsamedir -fileformat dds -dxt1
-	del %%O
+	"%batchDir%crunch.exe" -file "%%~O" -outsamedir -fileformat DDS -dxt1
+	del "%%O"
+	ren "%%~dpnO.dds" "%%~nO.DDS"
 )
 
 endlocal
