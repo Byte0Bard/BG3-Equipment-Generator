7ebe73b5bc2dd324648a2d9237f674b92eb99e8e
ByteBard0 <154865396+Byte0Bard@users.noreply.github.com>
2024-04-29 22:48:24 -0400

    Update to 1.5.0
    - Added hair generation
    - Added custom materials for hair generation
    - Added options for different shaders
    - Changed physical maps and mask maps to have color srgb set to "False"
    - Fixed .dds and .DDS being run through DDS converter
    - Fixed bug where tiefling fem could not be selected in equipment mesh body types
    - Fixed default boosts not saving or applying
    - Fixed bug where stats did not change if the weapon changed
    - Reworked stats to allow all equipment (except for Jewelry) to have tintable materials
    - Fixed an error where bounds information gives false warning for bounds

--- NoInstall/app/lib/crunch/bin/convertBG3.bat
+++ NoInstall/app/lib/crunch/bin/convertBG3.bat
@@ -14,15 +14,18 @@ set newName=""
 echo "Choice made: !folderPath!"
 folderPath = !folderPath!
 for %%W in ("!folderPath!"\*_BM.png "!folderPath!"\*_NM.png "!folderPath!"\*_BM.jpg "!folderPath!"\*_NM.jpg "!folderPath!"\*_BM.tga "!folderPath!"\*_NM.tga "!folderPath!"\*_BM.bmp "!folderPath!"\*_NM.bmp "!folderPath!"\*_BM.tiff "!folderPath!"\*_NM.tiff "!folderPath!"\*_BCM.png "!folderPath!"\*_BCM.jpg "!folderPath!"\*_BCM.tga "!folderPath!"\*_BCM.bmp "!folderPath!"\*_BCM.tiff "!folderPath!"\*icons.png "!folderPath!"\*_ICON.png "!folderPath!"\*_ICON.jpg "!folderPath!"\*_ICON.tga "!folderPath!"\*_ICON.bmp "!folderPath!"\*_ICON.tiff "!folderPath!"\*_MSKA.png "!folderPath!"\*_MSKA.jpg "!folderPath!"\*_MSKA.tga "!folderPath!"\*_MSKA.bmp "!folderPath!"\*_MSKA.tiff) do (
-    echo "Adding file to dxt5 list: %%~W"
-	set dxt5List=!dxt5List! "%%W"
-	set /a "totalFiles+=1"
-    
+    if /i not "%%~xW"==".dds" (
+		echo "Adding file to dxt5 list: %%~W"
+		set dxt5List=!dxt5List! "%%W"
+		set /a "totalFiles+=1"
+    )
 )
 for %%X in ("!folderPath!"\*_PM.png "!folderPath!"\*_PM.jpg "!folderPath!"\*_PM.tga "!folderPath!"\*_PM.bmp "!folderPath!"\*_PM.tiff "!folderPath!"\*_MSK.png "!folderPath!"\*_MSK.jpg "!folderPath!"\*_MSK.tga "!folderPath!"\*_MSK.bmp "!folderPath!"\*_MSK.tiff) do (
-    echo "Adding file to dxt1 list: %%~X"
-    set dxt1List=!dxt1List! "%%X"
-	set /a "totalFiles+=1"
+	if /i not "%%~xW"==".dds" (
+		echo "Adding file to dxt1 list: %%~X"
+		set dxt1List=!dxt1List! "%%X"
+		set /a "totalFiles+=1"
+	)
 ) 
 
 echo "Total Files @!totalFiles!"
